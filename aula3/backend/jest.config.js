08f4e0f11379e5e5c486b4dae69788df40d62567
andreluke <deco99885@gmail.com>
2024-09-11 19:55:26 -0300

    Adição das configurações dos testes

--- aula3/backend/jest.config.js
+++ aula3/backend/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    preset: '@shelf/jest-mongodb',
+    preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+      '^.+\\.ts?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    testMatch: ['**/?(*.)+(spec|test).ts'],
+    moduleDirectories: ['node_modules', 'src'],
   };
+  
