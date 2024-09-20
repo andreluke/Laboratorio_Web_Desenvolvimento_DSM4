aec9eb378b3d84f53d5502d129e346d3494fa2ec
andreluke <deco99885@gmail.com>
2024-09-19 22:15:23 -0300

    style: Adição de pastas para facilitar a compreensão do código

--- aula3/frontend/src/App.js
+++ aula3/frontend/src/App.js
@@ -1,67 +1,19 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import ClienteList from "./components/ClienteList";
 import ClienteForm from "./components/ClienteForm";
+import useClientes from "./hooks/useClient";
 import "./App.css";
 
 function App() {
-  const [clientes, setClientes] = useState([]);
-  const [editingCliente, setEditingCliente] = useState(null);
-
-  useEffect(() => {
-    fetchClientes();
-  }, []);
-
-  const fetchClientes = async () => {
-    try {
-      const response = await axios.get("http://localhost:3080/usuario");
-      setClientes(response.data);
-    } catch (error) {
-      console.error("Erro ao buscar clientes", error);
-    }
-  };
-
-  const addCliente = async (cliente) => {
-    try {
-      const response = await axios.post("http://localhost:3080/usuario", cliente);
-      setClientes([...clientes, response.data]);
-    } catch (error) {
-      console.error("Erro ao adicionar cliente", error);
-    }
-  };
-
-  const updateEmail = async (cliente) => {
-    try {
-      await axios.put(`http://localhost:3080/usuario/email`, {
-        id: cliente._id,
-        email: cliente.email,
-      });
-      fetchClientes();
-    } catch (error) {
-      console.error("Erro ao atualizar email", error);
-    }
-  };
-
-  const updateNome = async (cliente) => {
-    try {
-      await axios.put(`http://localhost:3080/usuario/nome`, {
-        id: cliente._id,
-        nome: cliente.nome,
-      });
-      fetchClientes();
-    } catch (error) {
-      console.error("Erro ao atualizar nome", error);
-    }
-  };
-
-  const deleteCliente = async (id) => {
-    try {
-      await axios.delete(`http://localhost:3080/usuario`, { data: { id } });
-      fetchClientes();
-    } catch (error) {
-      console.error("Erro ao deletar cliente", error);
-    }
-  };
+  const {
+    clientes,
+    addCliente,
+    updateEmail,
+    updateNome,
+    deleteCliente,
+    editingCliente,
+    setEditingCliente,
+  } = useClientes();
 
   return (
     <div className="App">
